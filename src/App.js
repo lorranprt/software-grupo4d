4a369b13c69a699740bf2b7c7694774630954599
Lorran <lorrangtech@gmail.com>
2023-11-07 19:45:52 -0300

    Components add

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import "leaflet/dist/leaflet.css"
 function App() {
   return (
     <div>
-        <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={true}>
+        <MapContainer center={[-22.935361, -43.215756]} zoom={13} scrollWheelZoom={true}>
         <TileLayer
     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
