6673d1e12394956674355d9ce93daa21a9c97309
Guilherme Vallim <guivallimaraujo@gmail.com>
2023-11-07 19:38:43 -0300

    App: Popup

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
-import { MapContainer, TileLayer, Marker} from "react-leaflet"
+import { MapContainer, TileLayer, Marker, Popup} from "react-leaflet"
 import "./App.css"
 import "leaflet/dist/leaflet.css" 
+import { Icon } from "leaflet";
 
 function App() {
   return (
     <div>
-        <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={true}>
+        <MapContainer center={[-22.935361, -43.215756]} zoom={13} scrollWheelZoom={true}>
         <TileLayer
     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
   />
-  <Marker position={[51.505, -0.09]}>
+  <Marker position={[-22.935361, -43.215756]} icon={new Icon({
+    iconRetinaUrl: require("leaflet/dist/images/marker-icon-2x.png"),
+    iconUrl: require("leaflet/dist/images/marker-icon.png"),
+    shadowUrl: require("leaflet/dist/images/marker-shadow.png")
+  })}>
+    <Popup>
+      Popup
+    </Popup>
   </Marker>
 </MapContainer>
     </div>
